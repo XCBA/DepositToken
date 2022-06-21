6b45b17f7274fde4f01b7cf017c3fc5abaef3fd5
DESKTOP-CT815J6\pioneer <oliverchang3030@gmail.com>
2022-06-21 20:55:31 +0900

    init project.

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -25,11 +25,11 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
 const config: HardhatUserConfig = {
   solidity: "0.8.4",
   networks: {
-    ropsten: {
-      url: process.env.ROPSTEN_URL || "",
-      accounts:
+    rinkeby: {
+      url: process.env.RINKEBY_URL || "",
+      accounts: 
         process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
-    },
+    }
   },
   gasReporter: {
     enabled: process.env.REPORT_GAS !== undefined,
